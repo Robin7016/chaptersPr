e5b5dd0a32338922ef030972a0862157f740c982
Robin636 <monika@rauner.de>
2023-10-22 17:44:58 +0200

    else:     # 22.10.23     call = Call.objects.create(textAnswered="")

--- chaptersProject/settings.py
+++ chaptersProject/settings.py
@@ -16,11 +16,10 @@ import dj_database_url
 
 from pathlib import Path
 
-#from chaptersProject import database # means database.py switch for environment
+# from chaptersProject import database # means database.py switch for environment
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -28,70 +27,69 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Add .env variables anywhere before SECRET_KEY
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
+  dotenv.load_dotenv(dotenv_file)
 
 # Update secret key
-SECRET_KEY = os.environ['SECRET_KEY']   #instead of your secret key
+SECRET_KEY = os.environ['SECRET_KEY']  # instead of your secret key
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-#-1fgo)47c)fdm-tr7e$o)!8bp-1b)t(9k&!#vd5_i162xzs9p'
-#SECRET_KEY = 'fp=4$h6(bz6t3esl5mo#g@mhp0u@3%80ew)oe(2il20@nxy9qx' seit 20.10.23
+# SECRET_KEY = 'django-insecure-#-1fgo)47c)fdm-tr7e$o)!8bp-1b)t(9k&!#vd5_i162xzs9p'
+# SECRET_KEY = 'fp=4$h6(bz6t3esl5mo#g@mhp0u@3%80ew)oe(2il20@nxy9qx' seit 20.10.23
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG see later
-#DEBUG = True
-#DEBUG = False
+# DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'chapters-dc88634a47a1.herokuapp.com']
-#ALLOWED_HOSTS = ['127.0.0.1']
-#ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['127.0.0.1']
+# ALLOWED_HOSTS = ['*']
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'corsheaders',
-    'chapters.apps.ChaptersConfig',
+  'django.contrib.admin',
+  'django.contrib.auth',
+  'django.contrib.contenttypes',
+  'django.contrib.sessions',
+  'django.contrib.messages',
+  'django.contrib.staticfiles',
+  'corsheaders',
+  'chapters.apps.ChaptersConfig',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  'django.middleware.security.SecurityMiddleware',
+  'whitenoise.middleware.WhiteNoiseMiddleware',
+  'django.contrib.sessions.middleware.SessionMiddleware',
+  'django.middleware.common.CommonMiddleware',
+  'django.middleware.csrf.CsrfViewMiddleware',
+  'django.contrib.auth.middleware.AuthenticationMiddleware',
+  'django.contrib.messages.middleware.MessageMiddleware',
+  'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'chaptersProject.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-      ,
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
+  {
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [BASE_DIR / 'templates']
+    ,
+    'APP_DIRS': True,
+    'OPTIONS': {
+      'context_processors': [
+        'django.template.context_processors.debug',
+        'django.template.context_processors.request',
+        'django.contrib.auth.context_processors.auth',
+        'django.contrib.messages.context_processors.messages',
+      ],
     },
+  },
 ]
 
 WSGI_APPLICATION = 'chaptersProject.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -104,48 +102,48 @@ WSGI_APPLICATION = 'chaptersProject.wsgi.application'
 # }
 
 IS_SQLITE_APP = False
-IS_HEROKU_APP = True    # if False then postgres-localdb
-DEBUG = True
+IS_HEROKU_APP = True  # if False then postgres-localdb
+DEBUG = False
 
 if IS_SQLITE_APP:
-    DEBUG = True
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+  DEBUG = True
+  DATABASES = {
+    'default': {
+      'ENGINE': 'django.db.backends.sqlite3',
+      'NAME': BASE_DIR / 'db.sqlite3',
     }
+  }
 else:
-    if IS_HEROKU_APP:
-        if DEBUG:
-            DATABASES = {
-                'default': {
-                    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-                    'NAME': 'd7lo27m0omlplf',
-                    'USER': 'ujmivcjgvatuwa',
-                    'PASSWORD': '56fe622a8b3bdc316dd24171f731396252c9af8d4c5cc3cab6a8974c1b49f341',
-                    'HOST': 'ec2-34-242-199-141.eu-west-1.compute.amazonaws.com',
-                    'PORT': '5432'
-                }
-            }
-        else:
-            DATABASES = {
-                "default": dj_database_url.config(
-                    conn_max_age=600,
-                    ssl_require=True,
-            )
-    }
-    else:
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.postgresql_psycopg2',
-                'NAME': 'chaptersdb',
-                'USER': 'postgres',
-                'PASSWORD': 'pages636',
-                'HOST': 'localhost',
-                'PORT': '5432'
-            }
+  if IS_HEROKU_APP:
+    if DEBUG:
+      DATABASES = {
+        'default': {
+          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+          'NAME': 'd7lo27m0omlplf',
+          'USER': 'ujmivcjgvatuwa',
+          'PASSWORD': '56fe622a8b3bdc316dd24171f731396252c9af8d4c5cc3cab6a8974c1b49f341',
+          'HOST': 'ec2-34-242-199-141.eu-west-1.compute.amazonaws.com',
+          'PORT': '5432'
         }
+      }
+    else:
+      DATABASES = {
+        "default": dj_database_url.config(
+          conn_max_age=600,
+          ssl_require=True,
+        )
+      }
+  else:
+    DATABASES = {
+      'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chaptersdb',
+        'USER': 'postgres',
+        'PASSWORD': 'pages636',
+        'HOST': 'localhost',
+        'PORT': '5432'
+      }
+    }
 
 # DATABASES = {
 #     'default': {
@@ -168,20 +166,19 @@ else:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+  {
+    'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+  },
+  {
+    'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+  },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -191,7 +188,6 @@ LANGUAGE_CODE = 'de'
 TIME_ZONE = 'Europe/Berlin'
 USE_I18N = True
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -210,9 +206,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = '/chapters/'
 LOGOUT_REDIRECT_URL = '/'
 
-SESSION_COOKIE_AGE = 28800   # 8 hours in seconds
+SESSION_COOKIE_AGE = 28800  # 8 hours in seconds
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)  #heroku
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)  #heroku
 
-#django_heroku.settings(locals())   #heroku
+# django_heroku.settings(locals())   #heroku
